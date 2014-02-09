e376a012319a1c17d44f549c279a4454defa0961
YumeYami <iscream.p@gmail.com>
2014-02-09 11:43:14 +0700

    NEWreq

--- src/Server/GroupUser.java
+++ src/Server/GroupUser.java
@@ -7,8 +7,43 @@ import Message.Message;
 
 public class GroupUser {
 	private String groupID;
-	private Vector<UserInfo> userInfos;
+	private ArrayList<UserInfo> userInfos;
 	private ArrayList<Message> messages;
 	private int totalUser;
-	private int lastMessageNumber;
+	private int lastStoreMsgNo;
+
+	public void GroupUser(String gID) {
+		groupID = gID;
+		totalUser = 0;
+		lastStoreMsgNo = 0;
+	}
+
+	public void joinGroup(String uID) {
+		UserInfo user1 = new UserInfo(uID);
+		userInfos.add(user1);
+		totalUser++;
+	}
+
+	public void leaveGroup(String uID) {
+		userInfos.remove(new UserInfo(uID));
+		totalUser--;
+	}
+
+	public void enterGroup(String uID) {
+		UserInfo user = userInfos.get(userInfos.indexOf(new UserInfo(uID)));
+		user.setOnline(true);
+		sendMsg(uID);
+	}
+
+	public void exitGroup(String uID) {
+		UserInfo user = userInfos.get(userInfos.indexOf(new UserInfo(uID)));
+		user.setOnline(false);
+	}
+
+	public void sendMsg(String uID) {
+		UserInfo user = userInfos.get(userInfos.indexOf(new UserInfo(uID)));
+		if (user.getLastMsgNo() > lastStoreMsgNo + messages.size())
+			return;
+
+	}
 }
